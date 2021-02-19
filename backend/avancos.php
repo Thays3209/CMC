76279ba55be58df27e397992a3e9ea060c706ec5
Thays Silva Santos <72770843+Thays3209@users.noreply.github.com>
2021-02-19 12:11:02 -0300

    Delete backend directory

--- backend/avancos.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require 'Connection.php';
-
-$consulta = $pdo->query("SELECT * FROM IMOV_ABAN_VER");
--- backend/sinalizacao.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require ('Connection.php');
-
-$idUserLogado = $_SESSION['id'];
-
-$consulta = $pdo->query("SELECT * FROM IMOV_ABAN_SIN WHERE ID_USUARIO = $idUserLogado");
